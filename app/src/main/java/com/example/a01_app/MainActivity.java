483c3004e47053a6e4ac9ed5f11d8bc78c09b67e
Javier <j.perezurrutia@gmail.com>
2023-05-31 19:55:06 -0400

    new project

---
+++ app/src/main/java/com/example/a01_app/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a01_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
